0a22c24a2464debaa0739da256dd68d6f2c8bcf7
Abhishek Vishwakarma <abhiv9255@gmail.com>
2025-06-17 01:40:52 +0530

    changes undone

--- backend/src/index.js
+++ backend/src/index.js
@@ -64,16 +64,12 @@ cron.schedule("0 * * * *", () => {
   }
 });
 
-// app.use("/api/users", userRoutes);
-// app.use("/api/admin", adminRoutes);
-// app.use("/api/auth", authRoutes);
-// app.use("/api/songs", songsRoutes);
-// app.use("/api/albums", albumsRoutes);
-// app.use("/api/stats", statsRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Hello from Vibely backend 🚀");
-});
+app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/songs", songsRoutes);
+app.use("/api/albums", albumsRoutes);
+app.use("/api/stats", statsRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
