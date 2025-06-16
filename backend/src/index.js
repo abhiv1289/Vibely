6e7e3a7fe5a61e3f3a5ca874d2f795df26b1a0df
Abhishek Vishwakarma <abhiv9255@gmail.com>
2025-06-17 01:28:28 +0530

    done

--- backend/src/index.js
+++ backend/src/index.js
@@ -72,10 +72,10 @@ app.use("/api/albums", albumsRoutes);
 app.use("/api/stats", statsRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../Frontend/dist/index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
   });
 }
 
